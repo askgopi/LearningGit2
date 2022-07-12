02326542d42796bb99013992be7a34068f242a74
askgopi <askgopi@outlook.com>
2022-07-12 05:17:39 -0500

    new line added

--- src/MyFirstJava.java
+++ src/MyFirstJava.java
@@ -2,5 +2,6 @@ public class MyFirstJava {
     public static void main(String[] args) {
         System.out.println("My Learning Git1");
         System.out.println("My Learning Git2");
+        System.out.println("My Learning Git3");
     }
 }
